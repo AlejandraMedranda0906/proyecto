305eece06e346ddf3dc8470982bcbeaf5b7ba6ba
Alejandra <146900779+AlejandraMedranda0906@users.noreply.github.com>
2024-01-23 22:11:57 -0500

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Tema1Component } from './components/tema1/tema1.component';
+import { Tema2Component } from './components/tema2/tema2.component';
+import { Tema3Component } from './components/tema3/tema3.component';
+import { Tema4Component } from './components/tema4/tema4.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, Tema1Component, Tema2Component, Tema3Component, Tema4Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/tema3/tema3.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tema3',
+  standalone: true,
+  imports: [],
+  templateUrl: './tema3.component.html',
+  styleUrl: './tema3.component.css'
+})
+export class Tema3Component {
+
+}
