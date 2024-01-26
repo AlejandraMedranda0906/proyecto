21daa7119fd8c33b338c52068e02ab3c708451ee
Alejandra <146900779+AlejandraMedranda0906@users.noreply.github.com>
2024-01-25 23:19:19 -0500

    c2
    c2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,11 @@ import { RouterOutlet } from '@angular/router';
 import { Tema1Component } from './components/tema1/tema1.component';
 import { Tema2Component } from './components/tema2/tema2.component';
 import { Tema3Component } from './components/tema3/tema3.component';
-import { Tema4Component } from './components/tema4/tema4.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, Tema1Component, Tema2Component, Tema3Component, Tema4Component],
+  imports: [CommonModule, RouterOutlet, Tema1Component, Tema2Component, Tema3Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
